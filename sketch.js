9f646eee1404b0edb9271cd829396b220c772137
Cristina Quito Vintimilla <mcquitovin@gmail.com>
2022-11-24 18:26:18 -0500

    prueba de particulas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,127 @@
+let pelotas = [];
+let angle = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+
+}
+function draw() {
+  background('rgba(200,100,30,0.01)');
+
+  for (let i = 0; i < pelotas.length; i++) {
+    if (pelotas[i].isAlife) {
+      pelotas[i].update();
+      pelotas[i].display();
+    } else {
+      pelotas.splice(i, 1);
+    }
+  }
 }
 
-function draw() {
-  circle(mouseX, mouseY, 20);
+function mouseClicked() {
+  for (let i = 0; i < 100; i++) {
+    background('rgba(200,100,30,0.01)');
+    let nuevaBola = new Bolita(mouseX, mouseY);
+    pelotas.push(nuevaBola);
+  }
+
+  print(pelotas.length)
+
+}
+
+
+//---------------------------
+//------- Clases ------------
+//---------------------------
+//------Random Walker--------
+
+class Bolita {
+  constructor(_mouseX, _mouseY) {
+    this.red = random(5, 25);
+    this.green = random(10, 250);
+    this.blue = random(10, 200);
+
+    this.t = 0;
+    this.tSpeed = random(0, 1);
+    this.noiseShift = random(1000);
+    this.lifespan = int(random(30, 200));
+
+    this.isAlife = true;
+
+    this.pos = createVector(_mouseX, _mouseY);
+    this.speed = createVector(random(-4, 4), random(-4, 4));
+    this.height = random(5, 20);
+    this.width = random(5, 20);
+    this.diametro = random(20, 60);
+
+    this.bolitaFinal = this.diametro / 1.5;
+
+    print('hola:viviere' + this.lifespan + 'frames');
+
+
+  }
+
+
+
+  update(_t) {
+
+    this.speed.rotate(map(noise(this.t + this.noiseShift), 0, 1, -0.1, 0.1));
+    this.pos.add(this.speed);
+    this.t += this.tSpeed;
+
+    translate(0);
+
+
+  }
+
+
+
+  display() {
+
+
+
+    rectMode(CENTER);
+    fill(this.blue, this.red, this.green);
+    rect(this.pos.x - 15, this.pos.y - 15, 10, 10);
+
+    stroke('rgba(0,0,0,0.2)');
+    strokeWeight(1);
+    fill(this.green, this.blue, this.red);
+    ellipse(this.pos.x, this.pos.y, this.diametro);
+
+    rectMode(CENTER);
+    fill(this.red, this.blue, this.green);
+    rect(this.pos.x + 20, this.pos.y - 20, 10, 10);
+
+    stroke('rgba(0,0,0,0.2)');
+    strokeWeight(1);
+    fill(this.red, this.blue, this.green);
+    ellipse(this.pos.x - 20, this.pos.y + 20, 10);
+
+    stroke('rgba(0,0,0,0.2)');
+    strokeWeight(1);
+    fill(this.blue, this.red, this.green);
+    ellipse(this.pos.x + 15, this.pos.y + 15, 10);
+
+
+    this.lifespan--;
+    if (this.lifespan <= 0) {
+      this.falleciending();
+    }
+
+
+  }
+
+  falleciending() {
+    this.diametro -= 0.4;
+    if (this.diametro <= 0) {
+      this.isAlife = false;
+      print('me morí, porque mi vida es ' + this.isAlife);
+      ellipse(this.pos.x, this.pos.y, this.bolitaFinal);
+
+
+    }
+  }
+
 }
